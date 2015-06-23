c90b2153084c263fbbf6fc9bec85e41e9a312d6e
zfh1005 <zfh1005@126.com>
2015-06-23 18:12:25 +0800

    add .whl package into project.
    add .whl package into project.
    The .whl file can be found at http://www.lfd.uci.edu/~gohlke/pythonlibs/

---
+++ Package/Install.bat
@@ -0,0 +1,7 @@
+pip install "numpy-1.9.2+mkl-cp27-none-win32.whl"
+pip install "numexpr-2.4.3-cp27-none-win32.whl"
+pip install "opencv_python-2.4.12-cp27-none-win32.whl"
+pip install "PyAudio-0.2.8-cp27-none-win32.whl"
+pip install "scipy-0.16.0-cp27-none-win32.whl"
+pip install "six-1.5.0-py2.py3-none-any.whl"
+@pause
